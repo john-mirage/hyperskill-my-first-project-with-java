bc6e7ce6e767ab8b1c70ebb36bc9cc9a706d3bed
John Mirage <28307090+john-mirage@users.noreply.github.com>
2024-06-22 13:27:16 +0200

    Update Main.java

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,30 +1,22 @@
 package calculator;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int bubblegum = 202;
-        int toffee = 118;
-        int iceCream = 2250;
-        int milkChocolate = 1680;
-        int doughnut = 1075;
-        int pancake = 80;
-        int sum = bubblegum + toffee + iceCream + milkChocolate + doughnut + pancake;
-        System.out.println("Earned amount:");
-        System.out.printf("Bubblegum: $%d%n", bubblegum);
-        System.out.printf("Toffee: $%d%n", toffee);
-        System.out.printf("Ice cream: $%d%n", iceCream);
-        System.out.printf("Milk chocolate: $%d%n", milkChocolate);
-        System.out.printf("Doughnut: $%d%n", doughnut);
-        System.out.printf("Pancake: $%d%n", pancake);
+        Item[] items = new Item[] {
+            new Item("Bubblegum", 202),
+            new Item("Toffee", 118),
+            new Item("Ice cream", 2250),
+            new Item("Milk chocolate", 1680),
+            new Item("Doughnut", 1075),
+            new Item("Pancake", 80)
+        };
+        Shop shop = new Shop(items);
+        shop.printItemsEarnedAmount();
         System.out.println();
-        System.out.printf("Income: $%d%n", sum);
-        System.out.println("Staff expenses:");
-        int staffExpenses = scanner.nextInt();
-        System.out.println("Other expenses:");
-        int otherExpenses = scanner.nextInt();
-        System.out.printf("Net income: $%d", sum - staffExpenses - otherExpenses);
+        int sumOfItemsEarnedAmount = shop.getSumOfItemsEarnedAmount();
+        System.out.printf("Income: $%d%n", sumOfItemsEarnedAmount);
+        int staffExpenses = UserInterface.askForExpenses("Staff expenses:");
+        int otherExpenses = UserInterface.askForExpenses("Other expenses:");
+        System.out.printf("Net income: $%d", sumOfItemsEarnedAmount - staffExpenses - otherExpenses);
     }
 }
